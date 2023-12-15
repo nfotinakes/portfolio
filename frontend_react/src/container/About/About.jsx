900c47e01bfde5601d487e5f5ca0fa5c0cefbe80
Nick <nfotinakes@csumb.edu>
2023-12-14 19:36:23 -0800

    Update about and connect sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,51 +1,71 @@
-import { motion } from 'framer-motion';
-import React, { useState, useEffect } from 'react';
-import './About.scss';
-import { images } from '../../constants';
+import { motion } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import "./About.scss";
+import { images } from "../../constants";
+import { urlFor, client } from "../../client";
 
-const abouts = [
-  { title: 'Web Development', description: 'I am a good web developer.', imgURL: images.about01},
-  { title: 'Web Design', description: 'I am a good web developer.', imgURL: images.about02},
-  { title: 'UI/UX', description: 'I am a good web developer.', imgURL: images.about03},
-  { title: 'Web Animations', description: 'I am a good web developer.', imgURL: images.about04}
-]
+// const abouts = [
+//   {
+//     title: "Web Development",
+//     description: "I am a good web developer.",
+//     imgURL: images.about01,
+//   },
+//   {
+//     title: "Web Design",
+//     description: "I am a good web developer.",
+//     imgURL: images.about02,
+//   },
+//   {
+//     title: "UI/UX",
+//     description: "I am a good web developer.",
+//     imgURL: images.about03,
+//   },
+//   {
+//     title: "Web Animations",
+//     description: "I am a good web developer.",
+//     imgURL: images.about04,
+//   },
+// ];
 
 function About() {
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
   return (
     <>
       <h2 className="head-text">
-        I Know That
-        <span>Good Design</span>
-        <br/>
-        means
-        <span>Good Business</span>
+        I Know That <span>Good Design</span>
+        <br /> means <span>Good Business</span>
       </h2>
 
       <div className="app__profiles">
-
         {abouts.map((about, index) => (
           <motion.div
-            whileInView={{opacity: 1}}
-            whileHover={{scale:1.1}}
-            transition={{duration: 0.5, type: 'tween'}}
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-item"
             key={about.title + index}
           >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
 
-            <img src={about.imgURL} alt={about.title}/>
-
-            <h2 className="bold-text" style={{marginTop: 20}}> {about.title} </h2>
-            <p className="p-text" style={{marginTop: 10}}> {about.description} </p>
-
-
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {" "}
+              {about.title}{" "}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {" "}
+              {about.description}{" "}
+            </p>
           </motion.div>
         ))}
-
-
-
       </div>
     </>
-  )
+  );
 }
 
 export default About;
